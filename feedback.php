600d702fe7643127436f89356308ce72199a7509
chalanadulajdilshan <chalanadulaj98@gmail.com>
2019-09-12 12:36:43 +0530

    #2 Create other files

---
+++ feedback.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Feedback - The Divine Villa</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <!-- Favicons -->
+        <link rel="shortcut icon" href="images/icons/favicon.png">
+
+        <!-- REVOLUTION STYLE SHEETS -->
+        <link rel="stylesheet" href="css/style.css"><!-- Style -->
+       
+
+    </head>
+    <body class="room archive">
+        <div id="preloading">
+            <div class="loading-icon">
+                <div class="sk-folding-cube">
+                    <div class="sk-cube1 sk-cube"></div>
+                    <div class="sk-cube2 sk-cube"></div>
+                    <div class="sk-cube4 sk-cube"></div>
+                    <div class="sk-cube3 sk-cube"></div>
+                </div>
+            </div>
+        </div>
+        <!-- Wrapper content -->
+        <div id="wrapper-container" class="content-pusher">
+            <div class="overlay-close-menu"></div>
+
+            <?php include './header.php' ?>
+            <!-- Main Content -->
+            <div id="main-content">
+                <div class="page-title">
+                    <div class="page-title-wrapper" data-stellar-background-ratio="0.5">
+                        <div class="content container">
+                            <h1 class="heading_primary">Feedback</h1>
+                            <ul class="breadcrumbs">
+                                <li class="item"><a href="./">Home</a></li>
+                                <li class="item"><span class="separator"></span></li>
+                                <li class="item active">Feedback</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="site-content container">
+                    <div class="container">
+                        <div class="row">
+                          
+                                <div class="col-md-4">
+                                    <div class="testimonial-item">
+                                        <div class="img-thumb">
+                                            <img src="images/blog/author1.jpg">
+                                        </div>
+                                        <div class="item-body">
+                                            <p>Praesent volutpat diam iaculis, fringilla orci vitae, hendrerit odio. Aenean venenatis, mauris et suscipit venenatis, augue lectus gravida dui, eget commodo mauris ex non risus. Vestibulum ultricies congue leo, quis pulvinar mi porttitor et. </p>
+                                        </div>  
+                                        <div class="item-title text-center">
+                                            <h4>Jone Smith</h4>
+                                            <span>Sri Lanka</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="testimonial-item">
+                                        <div class="img-thumb">
+                                            <img src="images/blog/author1.jpg">
+                                        </div>
+                                        <div class="item-body">
+                                            <p>Praesent volutpat diam iaculis, fringilla orci vitae, hendrerit odio. Aenean venenatis, mauris et suscipit venenatis, augue lectus gravida dui, eget commodo mauris ex non risus. Vestibulum ultricies congue leo, quis pulvinar mi porttitor et. </p>
+                                        </div>  
+                                        <div class="item-title text-center">
+                                            <h4>Jone Smith</h4>
+                                            <span>Sri Lanka</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="testimonial-item">
+                                        <div class="img-thumb">
+                                            <img src="images/blog/author1.jpg">
+                                        </div>
+                                        <div class="item-body">
+                                            <p>Praesent volutpat diam iaculis, fringilla orci vitae, hendrerit odio. Aenean venenatis, mauris et suscipit venenatis, augue lectus gravida dui, eget commodo mauris ex non risus. Vestibulum ultricies congue leo, quis pulvinar mi porttitor et. </p>
+                                        </div>  
+                                        <div class="item-title text-center">
+                                            <h4>Jone Smith</h4>
+                                            <span>Sri Lanka</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="testimonial-item">
+                                        <div class="img-thumb">
+                                            <img src="images/blog/author1.jpg">
+                                        </div>
+                                        <div class="item-body">
+                                            <p>Praesent volutpat diam iaculis, fringilla orci vitae, hendrerit odio. Aenean venenatis, mauris et suscipit venenatis, augue lectus gravida dui, eget commodo mauris ex non risus. Vestibulum ultricies congue leo, quis pulvinar mi porttitor et. </p>
+                                        </div>  
+                                        <div class="item-title text-center">
+                                            <h4>Jone Smith</h4>
+                                            <span>Sri Lanka</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="testimonial-item">
+                                        <div class="img-thumb">
+                                            <img src="images/blog/author1.jpg">
+                                        </div>
+                                        <div class="item-body">
+                                            <p>Praesent volutpat diam iaculis, fringilla orci vitae, hendrerit odio. Aenean venenatis, mauris et suscipit venenatis, augue lectus gravida dui, eget commodo mauris ex non risus. Vestibulum ultricies congue leo, quis pulvinar mi porttitor et. </p>
+                                        </div>  
+                                        <div class="item-title text-center">
+                                            <h4>Jone Smith</h4>
+                                            <span>Sri Lanka</span>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-4">
+                                    <div class="testimonial-item">
+                                        <div class="img-thumb">
+                                            <img src="images/blog/author1.jpg">
+                                        </div>
+                                        <div class="item-body">
+                                            <p>Praesent volutpat diam iaculis, fringilla orci vitae, hendrerit odio. Aenean venenatis, mauris et suscipit venenatis, augue lectus gravida dui, eget commodo mauris ex non risus. Vestibulum ultricies congue leo, quis pulvinar mi porttitor et. </p>
+                                        </div>  
+                                        <div class="item-title text-center">
+                                            <h4>Jone Smith</h4>
+                                            <span>Sri Lanka</span>
+                                        </div>
+                                    </div>
+                                </div>
+                           
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <!-- Footer -->
+            <?php include './footer.php' ?>
+        </div><!-- wrapper-container -->
+
+        <div id="back-to-top">
+            <i class="ion-ios-arrow-up" aria-hidden="true"></i>
+        </div>
+
+
+        <!-- Scripts -->
+        <script src="js/libs/jquery.min.js"></script><!-- jQuery -->
+        <script src="js/libs/stellar.min.js"></script><!-- parallax -->
+
+        <script src="js/libs/jquery-ui.min.js"></script><!-- ui -->
+        <script src="js/libs/daterangepicker.min.js"></script><!-- date -->
+        <script src="js/libs/daterangepicker.min-date.min.js"></script><!-- date2 -->
+        <script src="js/libs/bootstrap.min.js"></script><!-- Bootstrap -->
+        <script src="js/libs/smoothscroll.min.js"></script><!-- smoothscroll -->
+        <script src="js/libs/owl.carousel.min.js"></script><!-- Owl Carousel -->
+        <script src="js/libs/jquery.magnific-popup.min.js"></script><!-- Magnific Popup -->
+        <script src="js/libs/theia-sticky-sidebar.min.js"></script><!-- Sticky sidebar -->
+        <script src="js/libs/counter-box.min.js"></script><!-- counter -->
+        <script src="js/libs/jquery.thim-content-slider.min.js"></script><!-- Slider -->
+        <script src="js/theme-customs.js"></script><!-- Theme Custom -->
+
+    </body>
+
+</html>
